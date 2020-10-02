7c5d13b9c49b50f4abe97acd26b0298cc65c97d8
Andrew Ghostuhin <me@torinasakura.name>
2020-10-02 12:56:50 +0300

    refactor(next-app-with-apollo): change all apollo libs to @apollo/client
    affects: @atlantis-lab/next-app-with-apollo

--- packages/next-app-with-apollo/src/init-apollo.ts
+++ packages/next-app-with-apollo/src/init-apollo.ts
@@ -1,9 +1,7 @@
-import ApolloClient       from 'apollo-client'
-import fetch              from 'isomorphic-unfetch'
-import { InMemoryCache }  from 'apollo-cache-inmemory'
-import { ApolloLink }     from 'apollo-link'
-import { onError }        from 'apollo-link-error'
-import { createHttpLink } from 'apollo-link-http'
+import fetch                                       from 'isomorphic-unfetch'
+import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client'
+import { onError }                                 from '@apollo/client/link/error'
+import { createHttpLink }                          from '@apollo/client/link/http'
 
 interface Fetch {
   (uri, options: any, props: any): Promise<any>
