fec2c83a1b241914d224020447d75093b64c011d
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-16 00:22:46 +0500

    refactor(next-app-with-apollo): polished test file
    affects: @atlantis-lab/next-app-with-apollo

--- packages/next-app-with-apollo/__tests__/next-app-with-apollo.test.tsx
+++ packages/next-app-with-apollo/__tests__/next-app-with-apollo.test.tsx
@@ -1,33 +1,21 @@
-import App            from 'next/app'
-import React from 'react'
-import {compose,enhanced}        from 'recompose'
-import TestRenderer from 'react-test-renderer'
+// eslint-disable-next-line
+import App                   from 'next/app'
+import React          from 'react'
+import TestRenderer   from 'react-test-renderer'
+import { compose }    from 'recompose'
 
 import { withApollo } from '../src/with-apollo.provider'
 
-test('should initialize ApolloClient', function tester(){
+test('should initialize ApolloClient', function tester() {
+  const withProviders = compose(
+    withApollo({
+      uri: 'https://example.site',
+    })
+  )
 
+  const Wrapper = withProviders(() => <h1>Wrapped</h1>)
 
-    const withProviders = compose(
-        withApollo({
-            uri: 'https://example.site',
-            fetch: (uri, options, props) => {
-                if (props.token) {
-                    options.headers.authorization = props.token
-                }
-                if (typeof window !== 'undefined' && window.__NEXT_DATA__.props.token) {
-                    options.headers.authorization = window.__NEXT_DATA__.props.token
-                }
-                return fetch(uri, options)
-            },
-        })
-    )
+  const testRenderer = TestRenderer.create(<Wrapper />)
 
-
-    const Wrapper = withProviders(({ fetch }) => <h1>{'Wrapped'}</h1>)
-
-
-    const testRenderer = TestRenderer.create(<Wrapper/>)
-
-    expect(testRenderer.root.findByType('h1').props.children).toBe('Wrapped');
+  expect(testRenderer.root.findByType('h1').props.children).toBe('Wrapped')
 })
