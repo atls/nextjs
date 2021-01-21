97faf01d6489cd321d2922fa201caf6fa6b21caa
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-21 18:00:17 +0500

    fix(next-document-with-emotion): refactor test
    affects: @atlantis-lab/next-document-with-emotion
    
    - Changed global "test" script from "mctl test" to "lerna run test"
    
    ISSUES CLOSED: #151

--- packages/next-document-with-emotion/__tests__/with-emotion.test.tsx
+++ packages/next-document-with-emotion/__tests__/with-emotion.test.tsx
@@ -1,12 +1,35 @@
-import Document        from 'next/document'
-import { compose }     from 'recompose'
+import React                                      from 'react'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { compose }                                from 'recompose'
 
-import { withEmotion } from '../src/index'
+import { withEmotion }                            from '../src/index'
 
 describe('test suit for next-document-with-emotion', function describer() {
-  test('should check if document renders', function tester() {
+  test('should check if document initializes', function tester() {
+    const customDocTree = (
+      <Html>
+        <Head>
+          <title>Test title</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+
+    class MyDoc extends Document {
+      render() {
+        return customDocTree
+      }
+    }
+
     const withProviders = compose(withEmotion())
 
-    expect(`${withProviders(Document).render}`).toBeTruthy()
+    const Doc = withProviders(MyDoc)
+
+    const NewDoc = new Doc({ __NEXT_DATA__: { ids: ['12', '23', '34'] } })
+
+    expect(NewDoc.render()).toBe(customDocTree)
   })
 })
