2eacf3a1e605fde07776fc5d2e71b74b0efbcafb
Nikita Elfimov <ne@atls.team>
2021-05-20 20:14:01 +0500

    fix(next-app-with-apollo): satisfy typechecks

--- packages/next-app-with-apollo/src/with-apollo.provider.tsx
+++ packages/next-app-with-apollo/src/with-apollo.provider.tsx
@@ -14,85 +14,82 @@ interface Options {
   onUnauthenticated: () => void
 }
 
-export const withApollo = ({
-  uri,
-  headers = [],
-  resetStoreKeys = [],
-  onUnauthenticated,
-}: Options) => (PageComponent) => {
-  const WithApollo = ({ apolloClient, apolloState, apolloOptions, ...pageProps }) => {
-    let client
-    if (apolloClient) {
-      client = apolloClient
-    } else {
-      client = initApolloClient(apolloState, { ...apolloOptions, onUnauthenticated })
-    }
-
-    return (
-      <ApolloProvider client={client}>
-        <PageComponent {...pageProps} />
-      </ApolloProvider>
-    )
-  }
-
-  if (process.env.NODE_ENV !== 'production') {
-    const displayName = PageComponent.displayName || PageComponent.name || 'Component'
-    WithApollo.displayName = `withApollo(${displayName})`
-  }
-
-  WithApollo.getInitialProps = async (ctx) => {
-    const inAppContext = Boolean(ctx.ctx)
-
-    const apolloOptions = {
-      uri,
-      headers: extractHeaders(ctx, headers),
-    }
-
-    let pageProps = {}
-
-    const { apolloClient } = initOnContext(ctx, apolloOptions)
-
-    if (PageComponent.getInitialProps) {
-      pageProps = await PageComponent.getInitialProps(ctx)
-    } else if (inAppContext) {
-      pageProps = await App.getInitialProps(ctx)
-    }
-
-    if (typeof window === 'undefined') {
-      const { AppTree } = ctx
-
-      if (ctx.res && ctx.res.finished) {
-        return pageProps
+export const withApollo =
+  ({ uri, headers = [], resetStoreKeys = [], onUnauthenticated }: Options) =>
+  (PageComponent) => {
+    const WithApollo = ({ apolloClient, apolloState, apolloOptions, ...pageProps }) => {
+      let client
+      if (apolloClient) {
+        client = apolloClient
+      } else {
+        client = initApolloClient(apolloState, { ...apolloOptions, onUnauthenticated })
       }
 
-      if (AppTree) {
-        try {
-          const { getDataFromTree } = await import('@apollo/react-ssr')
+      return (
+        <ApolloProvider client={client}>
+          <PageComponent {...pageProps} />
+        </ApolloProvider>
+      )
+    }
 
-          let props
-          if (inAppContext) {
-            props = { ...pageProps, apolloClient }
-          } else {
-            props = { pageProps: { ...pageProps, apolloClient } }
-          }
+    if (process.env.NODE_ENV !== 'production') {
+      const displayName = PageComponent.displayName || PageComponent.name || 'Component'
+      WithApollo.displayName = `withApollo(${displayName})`
+    }
 
-          await getDataFromTree(<AppTree {...props} />)
-        } catch (error) {
-          // eslint-disable-next-line
-          console.error('Error while running `getDataFromTree`', error)
+    WithApollo.getInitialProps = async (ctx) => {
+      const inAppContext = Boolean(ctx.ctx)
+
+      const apolloOptions = {
+        uri,
+        headers: extractHeaders(ctx, headers),
+      }
+
+      let pageProps = {}
+
+      const { apolloClient } = initOnContext(ctx, apolloOptions)
+
+      if (PageComponent.getInitialProps) {
+        pageProps = await PageComponent.getInitialProps(ctx)
+      } else if (inAppContext) {
+        pageProps = await App.getInitialProps(ctx)
+      }
+
+      if (typeof window === 'undefined') {
+        const { AppTree } = ctx
+
+        if (ctx.res && ctx.res.finished) {
+          return pageProps
         }
 
-        Head.rewind()
+        if (AppTree) {
+          try {
+            const { getDataFromTree } = await import('@apollo/react-ssr')
+
+            let props
+            if (inAppContext) {
+              props = { ...pageProps, apolloClient }
+            } else {
+              props = { pageProps: { ...pageProps, apolloClient } }
+            }
+
+            await getDataFromTree(<AppTree {...props} />)
+          } catch (error) {
+            // eslint-disable-next-line
+            console.error('Error while running `getDataFromTree`', error)
+          }
+
+          Head.rewind()
+        }
+      }
+
+      return {
+        ...pageProps,
+        apolloOptions,
+        apolloState: apolloClient.cache.extract(),
+        apolloClient: ctx.apolloClient,
       }
     }
 
-    return {
-      ...pageProps,
-      apolloOptions,
-      apolloState: apolloClient.cache.extract(),
-      apolloClient: ctx.apolloClient,
-    }
+    return WithApollo
   }
-
-  return WithApollo
-}
