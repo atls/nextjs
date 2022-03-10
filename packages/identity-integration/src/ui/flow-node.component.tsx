4b695e1a8b54451ffaee7d932967dc4d981e55a5
Andrew Ghostuhin <me@torinasakura.name>
2022-03-10 18:39:35 +0300

    feat: identity integration values

--- packages/identity-integration/src/ui/flow-node.component.tsx
+++ packages/identity-integration/src/ui/flow-node.component.tsx
@@ -1,30 +1,37 @@
-import type { UiNodeInputAttributes } from '@ory/kratos-client'
-import type { UiNode }                from '@ory/kratos-client'
+import type { UiNode }  from '@ory/kratos-client'
 
-import { ReactElement }               from 'react'
-import { FC }                         from 'react'
-import { useMemo }                    from 'react'
+import { ReactElement } from 'react'
+import { FC }           from 'react'
+import { FormEvent }    from 'react'
+import { useCallback }  from 'react'
 
-import { useFlow }                    from '../providers'
+import { useFlowNode }  from '../providers'
+import { useValue }     from '../providers'
+
+type OnChangeCallback = (event: FormEvent<HTMLInputElement> | string | any) => void
 
 export interface FlowNodeProps {
   name: string
-  children: (node: UiNode) => ReactElement<any>
+  children: (node: UiNode, value: string | any, callback: OnChangeCallback) => ReactElement<any>
 }
 
 export const FlowNode: FC<FlowNodeProps> = ({ name, children }) => {
-  const { flow } = useFlow()
+  const node = useFlowNode(name)
+  const [value, setValue] = useValue(name)
 
-  const node = useMemo(
-    () =>
-      flow?.ui?.nodes?.find(
-        ({ attributes }) => (attributes as UiNodeInputAttributes).name === name
-      ),
-    [flow, name]
+  const onChange = useCallback(
+    (event: FormEvent<HTMLInputElement> | string | any) => {
+      if (event && event.target) {
+        setValue(event.target.value)
+      } else {
+        setValue(event)
+      }
+    },
+    [setValue]
   )
 
   if (node && typeof children === 'function') {
-    return children(node)
+    return children(node, value, onChange)
   }
 
   return null
