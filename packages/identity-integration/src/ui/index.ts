8ccc6ee7a9f00f92bc6395277920d8c90e1d18e5
Andrew Ghostuhin <me@torinasakura.name>
2022-03-12 17:54:21 +0300

    feat: flow nodes filter component

--- packages/identity-integration/src/ui/index.ts
+++ packages/identity-integration/src/ui/index.ts
@@ -1,5 +1,6 @@
-export * from './flow-nodes-group.component'
 export * from './flow-node-messages.component'
+export * from './flow-nodes-filter.component'
+export * from './flow-nodes-group.component'
 export * from './flow-messages.component'
 export * from './flow-submit.component'
 export * from './flow-node.component'
