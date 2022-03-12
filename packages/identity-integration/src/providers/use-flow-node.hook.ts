490234ba25226fa1d58d596d01c1f415cb6ba1a8
Andrew Ghostuhin <me@torinasakura.name>
2022-03-12 16:26:36 +0300

    feat(identity-integration): find nodes by id, also add values nodes allowed

--- packages/identity-integration/src/providers/use-flow-node.hook.ts
+++ packages/identity-integration/src/providers/use-flow-node.hook.ts
@@ -1,19 +1,28 @@
 import type { UiNodeInputAttributes } from '@ory/kratos-client'
+import type { UiNodeImageAttributes } from '@ory/kratos-client'
 import type { UiNode }                from '@ory/kratos-client'
 
 import { useMemo }                    from 'react'
 
 import { useFlow }                    from './use-flow.hook'
 
-export const useFlowNode = (name: string): UiNode | undefined => {
+export const useFlowNode = (nameOrId: string): UiNode | undefined => {
   const { flow } = useFlow()
 
   const node = useMemo(
     () =>
-      flow?.ui?.nodes?.find(
-        ({ attributes }) => (attributes as UiNodeInputAttributes).name === name
-      ),
-    [flow, name]
+      flow?.ui?.nodes?.find(({ attributes }) => {
+        if ((attributes as UiNodeInputAttributes).name) {
+          return (attributes as UiNodeInputAttributes).name === nameOrId
+        }
+
+        if ((attributes as UiNodeImageAttributes).id) {
+          return (attributes as UiNodeImageAttributes).id === nameOrId
+        }
+
+        return false
+      }),
+    [flow, nameOrId]
   )
 
   return node
