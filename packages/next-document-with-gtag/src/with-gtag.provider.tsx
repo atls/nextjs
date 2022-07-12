e9686380fb57d24b8dd18e090b9f31fe961d433d
Andrew Ghostuhin <me@torinasakura.name>
2022-07-12 11:24:55 +0300

    fix(gtag): update injectable code
    fix https://github.com/atls/nextjs/issues/237

--- packages/next-document-with-gtag/src/with-gtag.provider.tsx
+++ packages/next-document-with-gtag/src/with-gtag.provider.tsx
@@ -1,5 +1,5 @@
-import React    from 'react'
 import Document from 'next/document'
+import React    from 'react'
 
 export const withGtag = (gaTrackingId?: string) => (TargetComponent) =>
   class WithGtag extends TargetComponent {
@@ -8,21 +8,14 @@ export const withGtag = (gaTrackingId?: string) => (TargetComponent) =>
 
       if (gaTrackingId) {
         props.head.push(
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`} />
-        )
-
-        props.head.push(
           <script
             dangerouslySetInnerHTML={{
               __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag.pageview = function(info) {gtag('config', '${gaTrackingId}', info)}
-        gtag('js', new Date());
-        gtag.pageview({
-          page_path: window.location.pathname,
-          page_location: window.location.href,
-        });
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gaTrackingId}');
       `,
             }}
           />
