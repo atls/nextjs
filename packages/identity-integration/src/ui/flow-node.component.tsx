ef7ad63fd2ddfc6b5b13c087327168e53e94c6c0
Nikita Elfimov <me@tfk.name>
2023-01-12 03:33:44 +0500

    chore(identity-integration): patch ui node type

--- packages/identity-integration/src/ui/flow-node.component.tsx
+++ packages/identity-integration/src/ui/flow-node.component.tsx
@@ -1,18 +1,26 @@
-import type { UiNode }  from '@ory/kratos-client'
+import type { UiNode }                 from '@ory/kratos-client'
+import type { UiNodeAnchorAttributes } from '@ory/kratos-client'
 
-import { ReactElement } from 'react'
-import { FC }           from 'react'
-import { FormEvent }    from 'react'
-import { useCallback }  from 'react'
+import { ReactElement }                from 'react'
+import { FC }                          from 'react'
+import { FormEvent }                   from 'react'
+import { useCallback }                 from 'react'
 
-import { useFlowNode }  from '../providers'
-import { useValue }     from '../providers'
+import { useFlowNode }                 from '../providers'
+import { useValue }                    from '../providers'
 
 type OnChangeCallback = (event: FormEvent<HTMLInputElement> | string | any) => void
+type ActualUiNode = Omit<UiNode, 'attributes'> & {
+  attributes: Exclude<UiNode['attributes'], UiNodeAnchorAttributes>
+}
 
 export interface FlowNodeProps {
   name: string
-  children: (node: UiNode, value: string | any, callback: OnChangeCallback) => ReactElement<any>
+  children: (
+    node: ActualUiNode,
+    value: string | any,
+    callback: OnChangeCallback
+  ) => ReactElement<any>
 }
 
 export const FlowNode: FC<FlowNodeProps> = ({ name, children }) => {
@@ -31,7 +39,7 @@ export const FlowNode: FC<FlowNodeProps> = ({ name, children }) => {
   )
 
   if (node && typeof children === 'function') {
-    return children(node, value, onChange)
+    return children(node as ActualUiNode, value, onChange)
   }
 
   return null
